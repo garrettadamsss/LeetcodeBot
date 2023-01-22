5de2e2c40302f05bb9d2028c0db5afa9a96f59a6
Garrett Adams <Garrett Adams>
2023-01-21 23:16:36 -0800

    created new export and added differnt reply function

--- commands/problem.js
+++ commands/problem.js
@@ -1,47 +1,16 @@
 const { SlashCommandBuilder } = require("discord.js");
-
-async function WebScraper(){
-    //jquery implementation for nodejs that makes it easy to view dom elements
-    const cheerio = require('cheerio');
-    //controlls a headless browser which is needed for scraping dynamic websites
-    const puppeteer = require('puppeteer');
-
-    const randomPageNum = Math.floor(Math.random * 5);
-    const url = `https://leetcode.com/problemset/all/?page=${randomPageNum}`;
-
-    puppeteer
-    .launch()
-    .then(browser => browser.newPage())
-    .then(page => {
-        return page.goto(url)
-        .then(function() {
-            return page.content();
-        });
-    })
-    .then(html => {
-    const $ = cheerio.load(html);
-    //load all cells containing problem links into array of jquery objects
-    const problems = $('div[role = rowgroup] a'); 
-    //get random int 
-    let randomNum = Math.floor(Math.random() * problems.length + 1);
-
-    let problem = problems[randomNum].attribs.href;
-
-    const randomProblemLink = "https://leetcode.com".concat(problem);
-    console.log(`link: ${randomProblemLink}`);
-    return randomProblemLink; 
-    })
-    .catch(console.error);
-};
-
+const webscraper = require('../webscraper.js');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('problem')
         .setDescription('Returns random problem'),
     async execute(interaction) {
-        let problemLink = WebScraper();
-        await interaction.reply(problemLink);
-        console.log(`link from main: ${randomProblemLink}`);
+        await interaction.deferReply();
+        await wait(5000);
+        await interaction.editReply(webscraper());
+
+        
     },
 };
--- webscraper.js
+++ webscraper.js
@@ -1,38 +1,36 @@
-export default class WebScraper{
-    constructor(type){
-        this.type = type; 
-    }
-    getRandomProblem(){
-        //jquery implementation for nodejs that makes it easy to view dom elements
-        const cheerio = require('cheerio');
-        //controlls a headless browser which is needed for scraping dynamic websites
-        const puppeteer = require('puppeteer');
+module.exports = function WebScraper(){
+//jquery implementation for nodejs that makes it easy to view dom elements
+const cheerio = require('cheerio');
+//controlls a headless browser which is needed for scraping dynamic websites
+const puppeteer = require('puppeteer');
 
-        const randomPageNum = Math.floor(Math.random * 50);
-        const url = `https://leetcode.com/problemset/all/?page=${randomPageNum}`;
+const randomPageNum = Math.floor(Math.random * 50);
+const url = `https://leetcode.com/problemset/all/?page=${randomPageNum}`;
 
-        puppeteer
-        .launch()
-        .then(browser => browser.newPage())
-        .then(page => {
-            return page.goto(url)
-            .then(function() {
-                return page.content();
-            });
-        })
-        .then(html => {
-        const $ = cheerio.load(html);
-        //load all cells containing problem links into array of jquery objects
-        const problems = $('div[role = rowgroup] a'); 
-        console.log(problems.length);
-        //get random int 
-        let randomNum = Math.floor(Math.random() * problems.length + 1);
-
-        let problem = problems[randomNum].attribs.href;
-        console.log(problem);
-        const randomProblemLink = "https://leetcode.com".concat(problem);
-        console.log(randomProblemLink);
-        })
-        .catch(console.error);
-        }
+puppeteer
+.launch()
+.then(browser => browser.newPage())
+.then(page => {
+    return page.goto(url)
+    .then(function() {
+        return page.content();
+    });
+})
+.then(html => {
+    const $ = cheerio.load(html);
+    //load all cells containing problem links into array of jquery objects
+    const problems = [];
+    $('.truncate.overflow-hidden').each(function () {
+        problems.push($(this).find('a').attr('href'));
+    });
+    console.log(problems.length);
+    //get random int 
+    let randomNum = Math.floor(Math.random() * problems.length + 1);
+    //choose random random problem
+    let problemLink = problems[randomNum];
+    //combine problem path to create link
+    return "https://leetcode.com/".concat(problemLink);
+})
+.catch(console.error);
 };
+
